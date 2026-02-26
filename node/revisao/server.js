d84f0cbf4d38291f708a10a9d970afc6a88b1e05
Geleia <luizcanf@gmail.com>
2026-02-26 13:57:38 -0300

    Salva um vetor de nomes recebidos com o fs.writeFileSync()

--- node/revisao/server.js
+++ node/revisao/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -7,13 +8,17 @@ app.get('/', (req, res) => {
     res.render('pagina')
 })
 
+let nomes = []
 app.post('/ola', (req, res) => {
     let nome = req.body.nomezim
     if (typeof nome === 'undefined' || nome === '') {
         nome = 'mundo';
     }
     console.log(`Mensagem recebida de ${nome}!`)
+    nomes.push(nome)
+    fs.writeFileSync('dados/nomes.json', JSON.stringify(nomes))
     res.render('pagina', {fala: `Olá, ${nome}!`})
 })
 
-app.listen(37532)
+const PORT = 3000
+app.listen(PORT, console.log(`Servidor rodando em http://localhost:${PORT}`))
